84e7b8de43c5ecdaba14ca3489c0e554756aca9c
janfki <j.f.kismul@fys.uio.no>
2020-02-18 16:52:56 +0100

    population firing as kernel

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ import numpy as np
 import LFPy
 import pickle
 import matplotlib.pyplot as plt
+import time
+import scipy.signal as ss
 # </editor-fold>
-
+print("I'm not allowing individual neurons to receive both apical and basal inputs here, should I?")
+print('er dette en svakhet ved kernelmetoden, eller regner man med at en kjøring av mange nevroner med hver fyringsrate kan tilsvare ett nevron med påde api og basal?')
 # <editor-fold desc="Parse input">
 parser = argparse.ArgumentParser()
 parser.add_argument('-f',help='filtered:y/n')
@@ -37,41 +40,34 @@ if args.f != 'y' and args.b=='y':
     suffix='fn_by'
 
 if args.b == 'y':
-    fitses = pickle.load(open('data/fits_inhibition_off.p', 'rb'))
-    lines=pickle.load(open('data/lines_inhibition_off.p','rb'))
-else:
     fitses = pickle.load(open('data/fits_inhibition_on.p', 'rb'))
-    lines = pickle.load(open('data/lines_inhibition_on.p', 'rb'))
-
-# dist_I = dist_I[dist_I>2.5]
-# dist_E = dist_E[dist_E>2.]
-# dist_I = dist_I[dist_I<11]
-# test uten det over
+    lines=pickle.load(open('data/lines_inhibition_on.p','rb'))
+else:
+    fitses = pickle.load(open('data/fits_inhibition_off.p', 'rb'))
+    lines = pickle.load(open('data/lines_inhibition_off.p', 'rb'))
 # </editor-fold>
 
 # <editor-fold desc="make dir">
+folder_time = time.strftime("%m%d-%H%M")
 try:
-    os.makedirs(os.path.join('plots', suffix))
+    os.makedirs(os.path.join('plots', '{}_{}'.format(suffix,folder_time)))
 except OSError:
     pass
+FOLDER = os.path.join('plots', '{}_{}'.format(suffix,folder_time))
 # </editor-fold>
 
 # <editor-fold desc="Define functions">
-def insert_synapses(synparams, section, n, delay,heights):
+def insert_synapses(synparams, section, n,heights,firings):
     if section == 'dend':
         maxim = heights['basal_max']
         minim = heights['min']
         dist = dist_E
-        delay =delay['E']
-        # delay=np.random.normal(loc=delay['E'],scale=delay['std_E'])
-        firings_per_synapse = firings_per_synapse_E
+        firings_per_synapse = firings['firings_per_E_synapse']
     if section == 'apic':
         maxim = heights['max']
         minim = heights['apical_min']
         dist = dist_I
-        delay = delay['I']
-        # delay=np.random.normal(loc=delay['I'],scale=delay['std_I'])
-        firings_per_synapse = firings_per_synapse_I
+        firings_per_synapse = firings['firings_per_I_synapse']
     if section == 'allsec':
         maxim = heights['max']
         minim = heights['min']
@@ -80,95 +76,90 @@ def insert_synapses(synparams, section, n, delay,heights):
     idx = cell.get_rand_idx_area_norm(section=section, nidx=n, z_min=minim, z_max=maxim)
     # Insert synapses in an iterative fashion
     for i in idx:
-        ############################################
-        #hva om cell i velger en tid fra distr, og synapsene fyrer normalfordelt rundt den tiden
-        ############################################
         synparams.update({'idx': int(i)})
         # Create synapse(s) and setting times using the Synapse class in LFPy
         s = LFPy.Synapse(cell, **synparams)
-        s.set_spike_times(np.random.choice(dist, firings_per_synapse) + delay)
+        s.set_spike_times(np.random.choice(dist, firings_per_synapse))
 # </editor-fold>
 
 # <editor-fold desc="Define parameters & variables">
-parameters{
+
+# Define electrode geometry corresponding to a laminar electrode, where contact
+# points have a radius r, surface normal vectors N, and LFP calculated as the
+# average LFP in n random points on each contact:
+N = np.empty((16, 3))
+for i in range(N.shape[0]): N[i,] = [1, 0, 0] #normal unit vec. to contacts
+# put parameters in dictionary
+electrodeParameters = {
+    'sigma' : 0.3,              # Extracellular potential
+    'x' : np.zeros(16),# + 25,      # x,y,z-coordinates of electrode contacts
+    'y' : np.zeros(16),
+    'z' : np.linspace(-1600, -100, 16),
+    'n' : 20,
+    'r' : 10,
+    'N' : N,
+}
+
+
+
+parameters={
     'num_cells':300,
     'divi':2,
-    'n_syn':10,
-
+    'n_syn':200,
+    'firings':{
+        'firings_per_E_synapse':1,
+        'firings_per_I_synapse':1,
+    },
+    'heights':{
+        'max':1000.,
+        'min':-1000.,
+        'basal_max':-50.,
+        'apical_min':500.,
+    },
+    'cell_parameters':{
+        'morphology': morphology,
+        'cm': 1.0,  # membrane capacitance
+        'Ra': 150.,  # axial resistance
+        'v_init': -65.,  # initial crossmembrane potential
+        # 'passive': True,  # turn on NEURONs passive mechanism for all sections
+        'passive': False,  # turn on NEURONs passive mechanism for all sections
+        'passive_parameters': {'g_pas': 1. / 30000, 'e_pas': -65},
+        'nsegs_method': 'lambda_f',  # spatial discretization method
+        'lambda_f': 100.,  # frequency where length constants are computed
+        'dt': 2. ** -3,  # simulation time step size
+        'tstart': -300.,  # start time of simulation, recorders start at t=0
+        'tstop': 20.  # 50.,  # stop simulation at 100 ms.
+    },
+    'synapse_parameters':{
+        'idx': [],
+        # 'syntype': 'Exp2Syn',
+        'syntype': 'ExpSynI',
+        'tau1': 1.,
+        'tau2': 3.,
+        'tau':.1,
+        'weight': .1001,  # synaptic weight
+        'record_current': True,  # record synapse current
+    },
 }
-#PUTT ALLE DISSE I EN DICTIONARY, SÅ DET ER LETT Å LAGRE PARAM SOM SKAPER ET PLOTT
-# Define all parameters and variables used
-num_cells = 300
-divi = 2 #first 1/divi cells get basal input, rest apical
-cutoff = int(num_cells / divi)
-# n_syn = 200
-n_syn = 10
-# delay = {
-#     'E':0.,
-#     'I':0.,
-#     'T':0.,
-# }
-# std_factor = {
-#     'E':0.1,
-#     'I':0.1,
-#     'T':0.1,
-# }
-for i in ['E','I','T']:
-    delay.update({'std_{}'.format(i):std_factor[i]*delay[i]})
-heights = {
-    'max':1000.,
-    'min':-1000.,
-    'basal_max':-50.,
-    'apical_min':500.,
-}
-firings_per_synapse = 1
-firings_per_synapse_E = 1
-firings_per_synapse_I = 1
-
-tstart=-300.
-tstop = 20.  # bryuns-H has 20 and 25 for eeg, 40 for lfp and icsd
+cutoff = int(parameters['num_cells'] / parameters['divi'])
+print('cutoff at',cutoff,'of',parameters['num_cells'])
 p = [] #dipole moment
 v = [] #vmem
-x_line = np.linspace(0, tstop, len(fitses[0]))
-
-# Define cell parameters
-cell_parameters = {
-    'morphology': morphology,
-    'cm': 1.0,  # membrane capacitance
-    'Ra': 150.,  # axial resistance
-    'v_init': -65.,  # initial crossmembrane potential
-    'passive': True,  # turn on NEURONs passive mechanism for all sections
-    # 'passive': False,  # turn on NEURONs passive mechanism for all sections
-    'passive_parameters': {'g_pas': 1. / 30000, 'e_pas': -65},
-    'nsegs_method': 'lambda_f',  # spatial discretization method
-    'lambda_f': 100.,  # frequency where length constants are computed
-    'dt': 2. ** -3,  # simulation time step size
-    'tstart': tstart,  # start time of simulation, recorders start at t=0
-    'tstop': tstop  # 50.,  # stop simulation at 100 ms.
-}
-
-# Define synapse parameters
-synapse_parameters = {
-    'idx': [],
-    'syntype': 'Exp2Syn',
-    'tau1': 1.,
-    'tau2': 3.,
-    'weight': .001,  # synaptic weight
-    'record_current': True,  # record synapse current
-}
-
+x_line = np.linspace(0, parameters['cell_parameters']['tstop'], len(fitses[0]))
 # </editor-fold>
 
 # <editor-fold desc="Main simulation">
-for i in range(num_cells):
-    print('cell {} of {}.'.format(i + 1, num_cells), end='\r')
-    cell = LFPy.Cell(**cell_parameters)
+for i in range(parameters['num_cells']):
+    print('cell {} of {}.'.format(i + 1, parameters['num_cells']), end='\r')
+    cell = LFPy.Cell(**parameters['cell_parameters'])#**cell_parameters)
     cell.set_rotation(x=4.99, y=-4.33)  # let rotate around z-axis
 
     if i <= cutoff:
-        insert_synapses(synapse_parameters, 'dend', n_syn, delay,heights)
+        insert_synapses(parameters['synapse_parameters'], 'dend', parameters['n_syn']
+                        ,parameters['heights'],parameters['firings'])
     else:
-        insert_synapses(synapse_parameters, 'apic', n_syn, delay,heights)
+        insert_synapses(parameters['synapse_parameters'], 'apic', parameters['n_syn']
+                        ,parameters['heights'],parameters['firings'])
 
     cell.simulate(rec_imem=True, rec_vmem=True, rec_current_dipole_moment=True)
     P = cell.current_dipole_moment
@@ -178,14 +169,10 @@ print("                                 ", end='\r')
 print("done")
 # </editor-fold>
 
-# <editor-fold desc="Mean or Sum">
+# <editor-fold desc="Mean">
 bas = np.mean(p[0:cutoff], axis=0)[:, 0]
 api = np.mean(p[cutoff:], axis=0)[:, 0]
 signal_mean = np.mean(p, axis=0)[:, 0]
-
-# bas = np.sum(p[0:cutoff], axis=0)[:, 0]
-# api = np.sum(p[cutoff:], axis=0)[:, 0]
-# signal_mean = np.sum(p, axis=0)[:, 0]
 # </editor-fold>
 
 # <editor-fold desc="Scaling">
@@ -199,7 +186,17 @@ g=np.max(abs(api))
 h=np.max(abs(fitses[1]))
 i=g/h
 factors = [c, f, i]
+print(factors)
+# factors=[1,1,1]
+# print(factors)
 # </editor-fold>
+opp = []
+ned=[]
+for i in range(parameters['num_cells']):
+    if i<=cutoff:
+         opp.append(v[i])
+    else:
+        ned.append(v[i])
 
 # <editor-fold desc="Main plot">
 plt.figure()
@@ -208,15 +205,44 @@ plt.plot(lines[0], lines[1] * factors[0], 'k')
 
 plt.plot(cell.tvec, bas, label='dend', color='orange', ls='--')
 plt.plot(x_line, fitses[0] * factors[1], color='red', ls='-')
+vmemline = 65+np.mean(opp,axis=0)
+vl = np.max(vmemline)
+fi = np.max(fitses[0]*factors[1])
+plt.plot(cell.tvec,(fi/vl)*vmemline,'g')
 
 plt.plot(cell.tvec, api, label='apic', color='cyan', ls='--')
+plt.plot(x_line, fitses[1] * factors[2], color='blue', ls='-')
+vmemline=-65-np.mean(ned,axis=0)
+vl = np.min(vmemline)
+fi = np.min(fitses[1]*factors[2])
+plt.plot(cell.tvec,(fi/vl)*vmemline,'g')
+
+plt.xlabel('time [ms]')
+plt.ylabel('Voltage [some unit]')
+plt.title('{}'.format(suffix))
+plt.legend()
+plt.savefig('{}/signal_{}.pdf'.format(FOLDER,suffix))
+# </editor-fold>
+
+# <editor-fold desc="Main plot smoothed">
+smooth_sig = ss.savgol_filter(signal_mean,5,3)
+smooth_bas = ss.savgol_filter(bas,5,3)
+smooth_api = ss.savgol_filter(api,5,3)
+plt.figure()
+plt.plot(cell.tvec, smooth_sig, color='gray', ls='--', label='total')
+plt.plot(lines[0], lines[1] * factors[0], 'k')
+
+plt.plot(cell.tvec, smooth_bas, label='dend', color='orange', ls='--')
+plt.plot(x_line, fitses[0] * factors[1], color='red', ls='-')
+
+plt.plot(cell.tvec, smooth_api, label='apic', color='cyan', ls='--')
 plt.plot(x_line, fitses[1] * factors[2], color='blue', ls='-')
 
 plt.xlabel('time [ms]')
 plt.ylabel('Voltage [some unit]')
 plt.title('{}'.format(suffix))
 plt.legend()
-plt.savefig('plots/{}/signal_{}.pdf'.format(suffix,suffix))
+plt.savefig('{}/signal_smoothed_{}.pdf'.format(FOLDER,suffix))
 # </editor-fold>
 
 # <editor-fold desc = "Firing rate plots">
@@ -224,16 +250,16 @@ binny = np.linspace(0, 20, 200)
 plt.figure()
 plt.hist(dist_E, bins=binny, color='red')
 plt.title('{}'.format(suffix))
-plt.savefig('plots/{}/dend_{}.pdf'.format(suffix,suffix))
+plt.savefig('{}/dend_{}.pdf'.format(FOLDER,suffix))
 plt.figure()
 plt.hist(dist_I, bins=binny, color='blue')
 plt.title('{}'.format(suffix))
-plt.savefig('plots/{}/api_{}.pdf'.format(suffix,suffix))
+plt.savefig('{}/api_{}.pdf'.format(FOLDER,suffix))
 # </editor-fold>
 
 # <editor-fold desc="Vmem plot">
 fig,axs = plt.subplots(2)
-for i in range(num_cells):
+for i in range(parameters['num_cells']):
     if i<=cutoff:
         axs[0].plot(cell.tvec, v[i])
     else:
@@ -241,5 +267,10 @@ for i in range(num_cells):
 axs[0].set_title('basal')
 axs[1].set_title('apical')
 fig.suptitle('{}'.format(suffix))
-fig.savefig('plots/{}/vmems_{}.pdf'.format(suffix,suffix))
+fig.savefig('{}/vmems_{}.pdf'.format(FOLDER,suffix))
+# </editor-fold>
+
+# <editor-fold desc="dump param">
+with open('{}/parameters.txt'.format(FOLDER),'w') as f:
+    print(parameters,file=f)
 # </editor-fold>
